10712fc48f1c47f0ea2f9835263eed383647023a
limf <master_ker2008@sina.com>
2016-05-24 20:27:23 +0800

    密码加密

---
+++ src/main/java/com/alogic/csms/util/TransformPwd.java
@@ -0,0 +1,25 @@
+package com.alogic.csms.util;
+
+import com.anysoft.util.code.Coder;
+import com.anysoft.util.code.CoderFactory;
+
+public class TransformPwd {
+
+	public static void main(String[] args) {
+
+		Coder des3Coder = CoderFactory.newCoder("DES3");
+		Coder md5Coder = CoderFactory.newCoder("MD5");
+
+		String pwd = "123456";
+
+		String des3Pwd = des3Coder.encode(pwd, "1");
+		System.out.println("des3Pwd===" + des3Pwd);
+		System.out.println("pwd====" + des3Coder.decode(des3Pwd, "1"));
+
+		String md5Pwd = md5Coder.encode(pwd, "1");
+		System.out.println("md5Pwd===" + md5Pwd);
+		System.out.println("pwd====" + md5Coder.decode(md5Pwd, "1"));
+
+	}
+
+}
