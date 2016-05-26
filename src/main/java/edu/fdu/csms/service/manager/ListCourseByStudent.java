14a33a7bbeff6cf5139ee2484ef18dc247135138
limf <master_ker2008@sina.com>
2016-05-26 20:35:22 +0800

    服务权限控制测试

--- src/main/java/edu/fdu/csms/service/manager/ListCourseByStudent.java
+++ src/main/java/edu/fdu/csms/service/manager/ListCourseByStudent.java
@@ -10,13 +10,15 @@ import com.anysoft.util.Properties;
 import com.anysoft.util.PropertiesConstants;
 import com.logicbus.backend.Context;
 import com.logicbus.backend.ServantException;
+import com.logicbus.backend.Session;
+import com.logicbus.backend.SessionManager;
 import com.logicbus.backend.message.JsonMessage;
 import com.logicbus.dbcp.processor.Preprocessor;
 import com.logicbus.dbcp.sql.DBTools;
 import com.logicbus.models.servant.ServiceDescription;
 
 /**
- * 查询指定学生所选课程列表
+ * 教务员查看学生选课列表
  * 
  * @author limf
  */
@@ -29,6 +31,14 @@ public class ListCourseByStudent extends IDUBase {
 
 	@Override
 	protected void doIt(Context ctx, JsonMessage msg, Connection conn) throws Exception {
+
+		// 服务权限判断
+		Session session = SessionManager.get().getSession(ctx, false);
+		String role = session == null ? "anonymous" : session.hGet("user", "loginType", "anonymous");
+		if (!"manager".equals(role)) {
+			throw new ServantException("core.unauthorized", role + "用户无权访问本服务");
+		}
+
 		String studentNo = getArgument("studentNo", ctx);
 
 		sqlQuery = "SELECT c.course_id AS courseId,c.course_no AS courseNo,c.course_name AS courseName,sc.evaluation_grade AS evaluationGrade,sc.course_grade AS courseGrade FROM student_course_list sc JOIN course c ON sc.course_id = c.course_id WHERE sc.student_no = "
